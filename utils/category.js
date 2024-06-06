5403d260ed8e4e438854095e6e568d18db8a1d19
Jasonlinn <jasonlinn@example.com>
2024-06-07 01:23:07 +0800

    增加utils資料夾

---
+++ utils/category.js
@@ -0,0 +1,13 @@
+export const category = [{
+    id: 'play',
+    name: '玩',
+  },{
+    id: 'buy',
+    name: '買',
+  },{
+    id: 'live',
+    name: '住',
+  },{
+    id: 'traffic',
+    name: '行',
+  }];
