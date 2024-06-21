e2a2d2176e6184c8ec57d1bbb19bf9455faf2a34
Jasonlinn <jasonlinn@example.com>
2024-06-21 18:05:13 +0800

    增加channel id

--- server/api/webhook.post.js
+++ server/api/webhook.post.js
@@ -6,6 +6,7 @@ const config = {
 };
 // create LINE SDK client
 const bot = linebot({
+    channelId: process.env.CHANNEL_ID,
     channelSecret: process.env.CHANNEL_SECRET,
     channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
 });
