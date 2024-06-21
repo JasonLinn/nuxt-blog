78388992f19f2291315a00f175091b21ce71d43d
Jasonlinn <jasonlinn@example.com>
2024-06-21 16:29:23 +0800

    增加成功code

--- server/api/webhook.post.js
+++ server/api/webhook.post.js
@@ -1,3 +1,13 @@
 export default defineEventHandler((event) => {
     console.log(event, 'eeee')
+    try {
+        return {
+            statusCode: 200,
+        }
+      } catch (e) {
+        throw createError({
+          statusCode: 401,
+          statusMessage: 'Unauthorized'
+        })
+      }
 })
