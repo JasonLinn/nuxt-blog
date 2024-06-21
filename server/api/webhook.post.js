ec9891a192b84410210864766c89e171dbce4169
Jasonlinn <jasonlinn@example.com>
2024-06-21 17:17:31 +0800

    調整webhook code

--- server/api/webhook.post.js
+++ server/api/webhook.post.js
@@ -1,9 +1,26 @@
-export default defineEventHandler((event) => {
-    console.log(event, 'eeee')
+import line from ('@line/bot-sdk')
+
+// create LINE SDK config from env variables
+const config = {
+    channelSecret: process.env.CHANNEL_SECRET,
+};
+// create LINE SDK client
+const client = new line.messagingApi.MessagingApiClient({
+    channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
+  });
+
+export default defineEventHandler(async (event) => {
+    await line.middleware(config)
     try {
-        return {
-            statusCode: 200,
-        }
+        const body = await readBody(event)
+        // create an echoing text message
+        const echo = { type: 'text', text: body.message.text };
+
+        // use reply API
+        return client.replyMessage({
+            replyToken: event.replyToken,
+            messages: [echo],
+        });
       } catch (e) {
         throw createError({
           statusCode: 401,
