86fd4145d62e8ddbb7c1c38762e3c77d7bc398f7
Jasonlinn <jasonlinn@example.com>
2024-06-22 20:12:51 +0800

    增加拉資料庫顯示Flex訊息

--- server/api/webhook.post.js
+++ server/api/webhook.post.js
@@ -1,28 +1,55 @@
 import * as line from '@line/bot-sdk'
+import { pool } from '@/server/utils/db'
 
 // create LINE SDK client
 const client = new line.messagingApi.MessagingApiClient({
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
 });
 
-function handleEvent(event) {
+async function handleEvent (event) {
   if (event.type !== 'message' || event.message.type !== 'text') {
     // ignore non-text-message event
     return Promise.resolve(null);
   }
 
   let eventMsg = event.message.text;
-  let text = eventMsg;
-  if (eventMsg == '你好') {
-    text = '不好'
+  let flex = {
+    type: "flex",
+    altText: "優惠券列表",
+    contents: {
+      type: "bubble",
+      body: {
+        type: "box",
+        layout: "vertical",
+        contents: [
+          {
+            type: "text",
+            text: eventMsg
+          }
+        ]
+      }
+    }
   }
-  // create an echoing text message
-  const echo = { type: 'text', text };
+  if (eventMsg == '你好') {
+    flex.contents.body.contents = [{
+      type: 'text',
+      text: '很高興為您服務'
+    }]
+  }
+  if(eventMsg == '優惠券' || '優惠券列表') {
+    let cupon = await getCupon()
+    flex.contents.body.contents = cupon.map((item, index)=> {
+      return {
+        type: 'text',
+        text: index+1 + '.' + item.title
+      }
+    })
+  }
 
   // use reply API
   return client.replyMessage({
     replyToken: event.replyToken,
-    messages: [echo],
+    messages: [flex],
   });
 }
 
@@ -46,3 +73,17 @@ export default defineEventHandler(async (event) => {
         // })
       }
 })
+
+const getCupon = async () => {
+  const cuponRecords = await pool
+    .query(`SELECT * FROM "article"`)
+    .then((result) => result.rows)
+    .catch((error) => {
+      console.error(error)
+      throw createError({
+        statusCode: 500,
+        message: '無法取得優惠券列表，請稍候再試'
+      })
+    })
+  return cuponRecords
+}
