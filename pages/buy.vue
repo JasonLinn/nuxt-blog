aa313957a6725a3db62ac7ffca4fefcdf7d0b48b
Jasonlinn <jasonlinn@example.com>
2024-06-29 10:49:18 +0800

    增加Notify 程式碼

--- pages/buy.vue
+++ pages/buy.vue
@@ -1,3 +1,36 @@
 <template>
-    123
+    <div class="container">
+        <div class="row">
+            <textarea name="" id="" cols="30" rows="10" :value="text"></textarea>
+            <div class="btn btn-info" @click="sentNotify">送出</div>
+        </div>
+    </div>
 </template>
+
+<script setup>
+    let text = 123;
+    const sentNotify = () => {
+        alert(123)
+        useFetch('https://notify-api.line.me/api/notify', {
+        'headers': {
+            "Access-Control-Allow-Origin": "*",
+            // "Content-Type": "multipart/form-data",
+            // 'Sec-Fetch-Mode:': 'cors',
+            'Authorization': 'Bearer ' + '2aj6Y6IY0aiMAPL6LmCOlqHpsOCh7ieT7yNZ3j849kS',
+            "Access-Control-Allow-Methods": "*",
+            'Access-Control-Allow-Headers': '*',
+            // 'Accept': '*/*',
+            // 'Sec-Fetch-Site': 'same-origin',
+        },
+        'method': 'post',
+        'body': {
+            'message': 123
+        },
+        'payload': {
+        'message': 123,
+        'stickerPackageId': '2',
+        'stickerId': '523'
+            }
+        });
+    }
+</script>
