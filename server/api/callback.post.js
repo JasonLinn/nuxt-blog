9ae33049fc85a59b88631f9640539312689a19e5
Jasonlinn <jasonlinn@example.com>
2024-07-05 10:04:18 +0800

    更改notify連結

--- server/api/callback.post.js
+++ server/api/callback.post.js
@@ -1,11 +1,11 @@
 import axios from "axios"
 
-//點了驗證連結後進來https://notify-bot.line.me/oauth/authorize?response_type=code&scope=notify&response_mode=form_post&client_id=8jilqbG1VMeizXW9U36bH1&redirect_uri=https://446a-49-158-194-181.ngrok-free.app/api/callback/&state=f094a459-1d16-42d6-a709-c2b61ec53d60
+//點了驗證連結後進來https://notify-bot.line.me/oauth/authorize?response_type=code&scope=notify&response_mode=form_post&client_id=8jilqbG1VMeizXW9U36bH1&redirect_uri=https://nuxt-blog-swart.vercel.app/api/callback/&state=f094a459-1d16-42d6-a709-c2b61ec53d60
 export default defineEventHandler(async (event) => {
     const body = await readBody(event)
     console.log('New request: ' + getRequestURL(event), 'nnnnnnnnnnnnnnnnnnnnn')
     console.log(event, body, 'bbbbbbcccc')
-    await axios.post(`https://notify-bot.line.me/oauth/token?grant_type=authorization_code&code=${body.code}&redirect_uri=https://446a-49-158-194-181.ngrok-free.app/api/callback/&client_id=8jilqbG1VMeizXW9U36bH1&client_secret=a3G7L0gzZJ6vR9ALXRvLu21yMcqPZYb2cnTJbx9IhTn`, {
+    await axios.post(`https://notify-bot.line.me/oauth/token?grant_type=authorization_code&code=${body.code}&redirect_uri=https://nuxt-blog-swart.vercel.app/api/callback/&client_id=8jilqbG1VMeizXW9U36bH1&client_secret=a3G7L0gzZJ6vR9ALXRvLu21yMcqPZYb2cnTJbx9IhTn`, {
         // "headers": {
         //     // "grant_type": "authorization_code",
         //     // "code": body.code,
