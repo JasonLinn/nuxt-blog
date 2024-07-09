08b1d2c65013422a1df0f3b6dcb674cc2ab93204
JasonLinn <cheng11220@gmail.com>
2024-07-09 19:26:39 +0800

    暫時放code

---
+++ pages/user.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div id="userId">aaaa</div>
+            <textarea name="" id="" cols="30" rows="10" v-model="text"></textarea>
+            <div class="btn btn-info" @click="sentNotify">送出</div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+</script>
---
+++ server/api/user/[id].get.js
@@ -0,0 +1,25 @@
+import { pool } from '@/server/utils/db'
+
+export default defineEventHandler(async (event) => {
+  const user_id = getRouterParam(event, 'id')
+
+  const userRecord = await pool
+    .query('SELECT * FROM "user" WHERE "user_id" = $1;', [user_id])
+    .then((result) => result.rows?.[0])
+    .catch((error) => {
+      console.error(error)
+      throw createError({
+        statusCode: 500,
+        message: '無法取得優惠券，請稍候再試'
+      })
+    })
+
+  if (!userRecord) {
+    throw createError({
+      statusCode: 400,
+      message: '取得優惠券失敗，請稍候再試'
+    })
+  }
+  console.log(userRecord, 'ooooooo')
+  return userRecord
+})
