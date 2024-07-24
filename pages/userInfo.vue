0b2fe9f910d766e6200fa5066cca7116defa1d8b
Jasonlinn <jasonlinn@example.com>
2024-07-24 15:24:13 +0800

    修正屬性錯誤

--- pages/userInfo.vue
+++ pages/userInfo.vue
@@ -4,12 +4,12 @@
             <div id="userId">{{userName}}</div>
             <!-- <textarea name="" id="" cols="30" rows="10" v-model="text"></textarea> -->
             <div class="btn btn-info" @click="sentNotify">送出</div>
-            <div v-if="coupons">
-                <div v-for="coupon in coupons">
+            <div v-if="user">
+                <div v-for="coupon in user.coupons">
                     {{ JSON.parse(coupon).id }}
                 </div>
                 {{userId}}
-                {{ coupons }}
+                {{ user.coupons }}
             </div>
         </div>
     </div>
@@ -20,7 +20,7 @@ import useStore from "~~/store";
 const store = useStore();
 const userName = computed(() => store.getUserName);
 const userId = store.getUserId
-const { pending, data: coupons, error } = await useFetch(`/api/user/${userId}`)
+const { pending, data: user, error } = await useFetch(`/api/user/${userId}`)
 const items = [
   "{\"id\":35,\"title\":\"農場\",\"category\":\"buy\",\"content\":\"GOOD\",\"cover\":\"https://cc.tvbs.com.tw/img/program/upload/2021/12/29/20211229112130-d5a65e50.jpg\",\"amount\":9952,\"used_times\":0,\"hash\":[\"\"],\"updated_at\":\"2024-07-24T06:05:16.617Z\"}",
   "{\"id\":35,\"title\":\"農場\",\"category\":\"buy\",\"content\":\"GOOD\",\"cover\":\"https://cc.tvbs.com.tw/img/program/upload/2021/12/29/20211229112130-d5a65e50.jpg\",\"amount\":9951,\"used_times\":0,\"hash\":[\"\"],\"updated_at\":\"2024-07-24T06:21:04.378Z\"}"
