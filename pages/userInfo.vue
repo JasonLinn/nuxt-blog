45ef9a973edeeff87d37df31f2963f8a50597dc6
Jasonlinn <jasonlinn@example.com>
2024-07-24 12:38:33 +0800

    增加coupon 顯示

--- pages/userInfo.vue
+++ pages/userInfo.vue
@@ -4,6 +4,9 @@
             <div id="userId">{{userName}}</div>
             <!-- <textarea name="" id="" cols="30" rows="10" v-model="text"></textarea> -->
             <div class="btn btn-info" @click="sentNotify">送出</div>
+            <div v-for="coupon in coupons">
+                {{ coupon.id }}
+            </div>
         </div>
     </div>
 </template>
@@ -12,4 +15,5 @@
 import useStore from "~~/store";
 const store = useStore();
 const userName = computed(() => store.getUserName);
+const coupons = computed(() => store.getUserCoupons);
 </script>
--- store/index.js
+++ store/index.js
@@ -24,6 +24,7 @@ const useStore = defineStore("useStore", {
     getUserData: (state) => state.userData,
     getUserDisplayName: (state) => state.userData?.displayName,
     getUserId: (state) => state.userData?.userId,
+    getUserCoupons: (state) => state.userData?.coupons,
     getUserCover: (state) => state.userData?.pictureUrl,
     getUserStatus: (state) => state.userData?.statusMessage,
   },
