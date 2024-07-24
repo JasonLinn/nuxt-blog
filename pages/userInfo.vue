4a1fd9413651e7cc15778b3eb975ca238ea22690
Jasonlinn <jasonlinn@example.com>
2024-07-24 15:14:31 +0800

    增加userid

--- pages/userInfo.vue
+++ pages/userInfo.vue
@@ -8,6 +8,7 @@
                 {{ JSON.parse(coupon).id }}
             </div>
             {{userId}}
+            {{ coupons }}
         </div>
     </div>
 </template>
@@ -16,7 +17,7 @@
 import useStore from "~~/store";
 const store = useStore();
 const userName = computed(() => store.getUserName);
-const userId = computed(() => store.getUserId)
+const userId = store.getUserId
 const { pending, data: coupons, error } = await useFetch(`/api/user/${userId}`)
 const items = [
   "{\"id\":35,\"title\":\"農場\",\"category\":\"buy\",\"content\":\"GOOD\",\"cover\":\"https://cc.tvbs.com.tw/img/program/upload/2021/12/29/20211229112130-d5a65e50.jpg\",\"amount\":9952,\"used_times\":0,\"hash\":[\"\"],\"updated_at\":\"2024-07-24T06:05:16.617Z\"}",
