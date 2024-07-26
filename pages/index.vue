0cf1559e125c9c41fe75f8268ddd7a9cc3fd88b9
Jasonlinn <jasonlinn@example.com>
2024-07-26 23:32:53 +0800

    固定input scale

--- pages/index.vue
+++ pages/index.vue
@@ -30,7 +30,7 @@
         </span>
       </div>
       <div class="search">
-        <input type="text" class="searchInput" placeholder="請輸入優惠券名稱" v-model="searchText">
+        <input type="text" class="searchInput" maximum-scale="1" placeholder="請輸入優惠券名稱" v-model="searchText">
         <svg  xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search search-icon" viewBox="0 0 16 16">
           <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
         </svg>
