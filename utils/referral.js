976c66cfc9f63539b307c92146a3ef278feb05f3
Jasonlinn <jasonlinn@example.com>
2024-07-27 15:49:02 +0800

    增加url query 辨別推薦店家

---
+++ utils/referral.js
@@ -0,0 +1,13 @@
+export const referral = [{
+    code: 'jinbnb',
+    name: '衿日林民宿',
+  },{
+    code: 'okbnb',
+    name: 'OK民宿',
+  },{
+    id: 'live',
+    name: '住',
+  },{
+    id: 'traffic',
+    name: '行',
+  }];
