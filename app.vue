35bdaccfb44983641d62a2f3c155cb72bcf0106d
Jasonlinn <jasonlinn@example.com>
2024-07-27 15:53:13 +0800

    調整SEO名稱

--- app.vue
+++ app.vue
@@ -8,7 +8,7 @@
 
 <script setup>
 useHead({
-  titleTemplate: (title) => (title ? `${title} - 宜蘭旅遊通-電子化票券` : '宜蘭旅遊通-電子化票券'),
+  titleTemplate: (title) => (title ? `${title} - 宜蘭旅遊通-電子優惠券` : '宜蘭旅遊通-電子優惠券'),
   htmlAttrs: {
     lang: 'zh-TW'
   },
@@ -24,7 +24,7 @@ const { origin } = useRequestURL()
 const { baseURL } = useRuntimeConfig().app
 
 const seoMeta = {
-  title: '宜蘭旅遊通-電子化票券',
+  title: '宜蘭旅遊通-電子優惠券',
   description: '歡迎使用，內含各式優惠',
   image: 'https://www.shutterstock.com/zh-Hant/blog/wp-content/uploads/sites/11/2023/02/0123-cover.png',
   url: `${origin}${baseURL}`
