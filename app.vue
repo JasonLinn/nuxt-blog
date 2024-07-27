0ac1975087889c47cf5f44403c31abf2bf9af09b
Jasonlinn <jasonlinn@example.com>
2024-07-27 17:17:00 +0800

    更改url縮圖顯示

--- app.vue
+++ app.vue
@@ -26,7 +26,7 @@ const { baseURL } = useRuntimeConfig().app
 const seoMeta = {
   title: '宜蘭旅遊通-電子優惠券',
   description: '歡迎使用，內含各式優惠',
-  image: 'https://www.shutterstock.com/zh-Hant/blog/wp-content/uploads/sites/11/2023/02/0123-cover.png',
+  image: origin + '/logo.png',
   url: `${origin}${baseURL}`
 }
 
