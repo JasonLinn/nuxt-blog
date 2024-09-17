ec883bfd497349127009e2f3be62c71e6ceef079
Jasonlinn <jasonlinn@example.com>
2024-09-18 01:33:46 +0800

    增加頁面轉場

--- app.vue
+++ app.vue
@@ -43,3 +43,13 @@ useSeoMeta({
   twitterCard: 'summary_large_image'
 })
 </script>
+<style>
+/* .layout-enter-active,
+.layout-leave-active {
+  transition: all 0.4s;
+}
+.layout-enter-from,
+.layout-leave-to {
+  filter: grayscale(1);
+} */
+</style>
