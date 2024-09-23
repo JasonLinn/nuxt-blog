bf6ee561b6841557f8f5f1e860df62a54b119b12
Jasonlinn <jasonlinn@example.com>
2024-09-23 12:51:41 +0800

    補上 static.js

---
+++ pages/test.vue
@@ -0,0 +1,12 @@
+<template>
+    <carousel :items-to-show="1.5">
+    <slide v-for="slide in 10" :key="slide">
+      {{ slide }}
+    </slide>
+
+    <template #addons>
+      <navigation />
+      <pagination />
+    </template>
+  </carousel>
+</template>
---
+++ utils/static.js
@@ -0,0 +1,1 @@
+export const index_liff_url = 'https://liff.line.me/2005661804-zld9QenV'
