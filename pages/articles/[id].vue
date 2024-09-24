017e589a9940ecbd6d48a824f16b8aa510b32680
Jasonlinn <jasonlinn@example.com>
2024-09-24 20:35:22 +0800

    調整單字logo位置

--- pages/articles/[id].vue
+++ pages/articles/[id].vue
@@ -421,7 +421,8 @@ const getCupon = () => {
                       "size": "xxl",
                       "url": "https://nuxt-blog-swart.vercel.app/one_text.png",
                       "position": "absolute",
-                      "offsetEnd": "10px"
+                      "offsetEnd": "10px",
+                      "offsetBottom": "2px"
                     },
                   ]
                 },
