298c7fc599230d21acb1603612fae502e753ad7d
Jasonlinn <jasonlinn@example.com>
2024-10-18 22:31:55 +0800

    調整LINE icon高度

--- components/RelativeFooter.vue
+++ components/RelativeFooter.vue
@@ -2,7 +2,7 @@
     <footer>
         <hr>
         如有興趣可洽「宜蘭旅遊通」官方LINE
-        <a href="https://lin.ee/6ymvmWV"><img src="https://scdn.line-apps.com/n/line_add_friends/btn/zh-Hant.png" alt="加入好友" height="36" border="0"></a>
+        <a href="https://lin.ee/6ymvmWV"><img src="https://scdn.line-apps.com/n/line_add_friends/btn/zh-Hant.png" alt="加入好友" height="18" border="0"></a>
         ，或消費店家😊，可以取得獨家優惠!
     </footer>
 </template>
