66bb687a95824d0433d4e2d5cc9e8407b4b42fdb
Jasonlinn <jasonlinn@example.com>
2024-10-24 01:41:38 +0800

    增加liff id

--- server/utils/liffID.js
+++ server/utils/liffID.js
@@ -1,5 +1,6 @@
 export const LIFF_ID = {
     'index': '2005661804-zld9QenV',
+    'articles-id': '2005661804-zld9QenV',
     'relative': '2005661804-9KNJXMpB',
     'userInfo': '2005661804-RoPMzgkN'
 }
