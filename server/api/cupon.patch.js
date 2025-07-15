11208ad776f7ecfec6cefd5cdadef2e6fad6deed
jasonlinn <cheng11220@gmail.com>
2025-07-15 08:11:54 +0800

    修正資料庫路徑(優惠券)

--- server/api/cupon.patch.js
+++ server/api/cupon.patch.js
@@ -1,9 +1,18 @@
-import { pool } from '../utils/db.js'
+import pkg from 'pg'
+const { Pool } = pkg
+
+// 優惠券專用資料庫連線
+const couponPool = new Pool({
+  connectionString: 'postgresql://nuxt-marketing_owner:ys7ZNVhOrg9c@ep-rough-voice-a1ele0z6-pooler.ap-southeast-1.aws.neon.tech/nuxt-marketing?sslmode=require&channel_binding=require',
+  ssl: {
+    rejectUnauthorized: false
+  }
+})
 
 export default defineEventHandler(async (event) => {
   const body = await readBody(event)
 
-  const articleRecord = await pool
+  const articleRecord = await couponPool
     .query(
       'UPDATE "article" SET "amount" = $1, "updated_at" = NOW() WHERE "id" = $2 RETURNING *;',
       [body.amount, body.id]
--- server/api/received.post.js
+++ server/api/received.post.js
@@ -1,9 +1,18 @@
-import { pool } from '@/server/utils/db'
+import pkg from 'pg'
+const { Pool } = pkg
+
+// 優惠券專用資料庫連線
+const couponPool = new Pool({
+  connectionString: 'postgresql://nuxt-marketing_owner:ys7ZNVhOrg9c@ep-rough-voice-a1ele0z6-pooler.ap-southeast-1.aws.neon.tech/nuxt-marketing?sslmode=require&channel_binding=require',
+  ssl: {
+    rejectUnauthorized: false
+  }
+})
 
 export default defineEventHandler(async (event) => {
   const body = await readBody(event)
 
-  const receivedRecord = await pool
+  const receivedRecord = await couponPool
     .query('INSERT INTO "received" ("coupon_title", "coupon_id", "coupon_content", "user_id", "user_name", "remark", "received_time") VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *;', [
       body.coupon_title,
       body.coupon_id,
